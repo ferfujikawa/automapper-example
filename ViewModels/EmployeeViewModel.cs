30c3ec24cbf2b27991905740bfc5a084d2f492f3
fujikawa <?=utfpr#01brasil=?>
2023-08-22 08:22:50 -0300

    Criação da ViewModel de Employee.

---
+++ ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace automapper_example.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string DepartmentName { get; private set; }
+
+        public EmployeeViewModel(Guid id, string firstName, string lastName, string departmentName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            DepartmentName = departmentName;
+        }
+    }
+}
