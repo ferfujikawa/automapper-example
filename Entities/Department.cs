c492ee1e56163b69bfe1d580c3ec28cca0775d7a
fujikawa <?=utfpr#01brasil=?>
2023-08-22 08:11:04 -0300

    Criação das entidades e value objects.

---
+++ Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace automapper_example.Entities
+{
+    public class Department
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Department(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using automapper_example.ValueObjects;
+
+namespace automapper_example.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; private set; }
+        public Name Name { get; private set; }
+        public Department Department { get; private set; }
+
+        public Employee(Name name, Department department)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Department = department;
+        }
+    }
+}
