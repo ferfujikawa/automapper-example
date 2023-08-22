e9aac10ea1e5d7ce24a97acf68128ced69a92a65
fujikawa <?=utfpr#01brasil=?>
2023-08-22 08:30:29 -0300

    Inclusão do pacote AutoMapper. Criação do profile de Employee.

---
+++ Entities/Profiles/EmployeeProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using automapper_example.ViewModels;
+
+namespace automapper_example.Entities.Profiles
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Employee, EmployeeViewModel>()
+                .ForMember(
+                    r => r.FirstName,
+                    opt => opt.MapFrom(x => x.Name.FirstName))
+                .ForMember(
+                    r => r.LastName,
+                    opt => opt.MapFrom(x => x.Name.LastName))
+                .ForMember(
+                    r => r.DepartmentName,
+                    opt => opt.MapFrom(x => x.Department.Name));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # automapper-example
 
 Projeto para prática de uso do framework [AutoMapper](https://automapper.org/).
+
+## Instalação do pacote AutoMapper
+
+```bash
+Install-Package AutoMapper.Extensions.Microsoft.DependencyInjection
+```
